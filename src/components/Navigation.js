b01c8e310c6bfdd28b565b958229704205ee9583
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 11:41:20 -0800

    Nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+
+const Styles = styled.div`
+  .navbar { background-color: #222; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
+
+export const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Pursuit of Hackiness</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Form className="form-center">
+        <FormControl type="text" placeholder="Search" className="" />
+      </Form>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="/Game">Game</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
