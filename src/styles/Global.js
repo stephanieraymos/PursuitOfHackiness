ddc8fcdeefae0397f9493bad42929440543c1bf4
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 12:10:06 -0800

    Global styles

---
+++ src/styles/Global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+
+  *,
+  *::after,
+  *::before {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 60%;
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: 'Anton', sans-serif;
+  }
+`;
