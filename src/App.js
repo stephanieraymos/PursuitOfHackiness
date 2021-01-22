6e7858f2b01505a4c77cd80916c88c0e86d3dedf
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 11:56:14 -0800

    Routes and imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation } from './components/Navigation';
+import { Navigation } from "./components/Navigation";
+import Landing from './components/Landing';
+import Home from './components/Home';
+import Bootcamp from './components/Bootcamp';
+import Career from './components/Career';
 
 function App() {
   return (
-<>
-  <Router>
-    <Navigation />
-  </Router>
-</>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/Landing" component={Landing} />
+          <Route exact path="/Home" component={Home} />
+          <Route path="/Bootcamp" component={Bootcamp} />
+          <Route path="/Career" component={Career} />
+        </Switch>
+        <Navigation />
+      </Router>
+    </>
   );
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+`;
 
 function Landing() {
-    return (
-        <div>
-            <h1>Welcome to Pursuit of Hackiness
-</h1>
-        </div>
-    )
+  return (
+    <div>
+      <GridWrapper>
+        <p>About the game</p>
+        <p>How it works</p>
+      </GridWrapper>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
