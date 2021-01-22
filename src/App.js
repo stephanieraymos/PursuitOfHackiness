12961236746f79748963a445d20ebf52e5680f2e
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 12:14:48 -0800

    Setting up hook to handle navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation } from "./components/Navigation";
+import { Navigation } from "./components/navbar/Navigation";
+import GlobalStyle from './styles/Global';
 import Landing from './components/Landing';
 import Home from './components/Home';
 import Bootcamp from './components/Bootcamp';
 import Career from './components/Career';
+
+const [isNavbarOpen, setIsNavbarOpen] = useState('false');
+
+handleNavbar = () => {
+  setIsNavbarOpen(!isNavbarOpen)
+}
 
 function App() {
   return (
