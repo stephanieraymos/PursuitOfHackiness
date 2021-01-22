2e27003ed43672b712a7065059bb8c0b188a133c
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 11:15:59 -0800

    Landing page

---
+++ src/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div>
+            <h1>Welcome to Pursuit of Hackiness
+</h1>
+        </div>
+    )
+}
+
+export default Landing
