30dbdb1db801a25f28d2ee0cd43d30f63161acaa
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-22 12:17:37 -0800

    Functioning

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
+import { useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation } from "./components/navbar/Navigation";
-import GlobalStyle from './styles/Global';
-import Landing from './components/Landing';
-import Home from './components/Home';
-import Bootcamp from './components/Bootcamp';
-import Career from './components/Career';
-
-const [isNavbarOpen, setIsNavbarOpen] = useState('false');
-
-handleNavbar = () => {
-  setIsNavbarOpen(!isNavbarOpen)
-}
+import GlobalStyle from "./styles/Global";
+import Landing from "./components/Landing";
+import Home from "./components/Home";
+import Bootcamp from "./components/Bootcamp";
+import Career from "./components/Career";
 
 function App() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState("false");
+
+  // handleNavbar = () => {
+  //   setIsNavbarOpen(!isNavbarOpen);
+  // };
+
   return (
     <>
       <Router>
+        <Navigation />
         <Switch>
           <Route path="/Landing" component={Landing} />
           <Route exact path="/Home" component={Home} />
           <Route path="/Bootcamp" component={Bootcamp} />
           <Route path="/Career" component={Career} />
         </Switch>
-        <Navigation />
       </Router>
     </>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const GridWrapper = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  margin-top: 1em;
-  margin-left: 6em;
-  margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
-`;
+function Landing() {
+  const GridWrapper = styled.div`
+    display: grid;
+    grid-gap: 10px;
+    margin-top: 1em;
+    margin-left: 6em;
+    margin-right: 6em;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: minmax(25px, auto);
+  `;
 
-export default Landing = (props) => (
-  <GridWrapper>
-    <p>About the game</p>
-    <p>How it works</p>
-  </GridWrapper>
-)
+  return (
+    <div>
+      <GridWrapper>
+        <p>About the game</p>
+        <p>How it works</p>
+      </GridWrapper>
+    </div>
+  );
+}
+
+export default Landing;
