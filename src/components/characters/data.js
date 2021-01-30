f72d251a66dab0fd0df959580f94617af4224a17
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-29 18:59:56 -0800

    data

---
+++ src/components/characters/data.js
@@ -0,0 +1,28 @@
+const characters = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image: "",
+  },
+  {
+    id: 2,
+    name: "john johnson",
+    job: "web designer",
+    image: "",
+  },
+  {
+    id: 3,
+    name: "peter peterson",
+    job: "intern",
+    image: "",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image: "",
+  },
+];
+
+export default characters;
