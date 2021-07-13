1b787ba658a71d3d446b7254f99a24894d060a5a
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-13 15:12:18 -0700

    Character headers

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -26,39 +26,86 @@ function Character() {
       </section> */}
         <h1 className="center-text p-top">Characters</h1>
         <div className="container">
-          <img src="/img/superhero.png"
-          alt="character"
-          className="character-small"
-        />
-        <img
-          src="/img/pencil.png"
-          alt="character"
-          className="character-small"
-        />
-        <img src="/img/ninja.png" alt="character" className="character-small" />
-        <img
-          src="/img/native-american.png"
-          alt="character"
-          className="character-small"
-        />
-        <img
-          src="/img/mythical-creature.png"
-          alt="character"
-          className="character-small"
-        />
-        <img src="/img/ghost.png" alt="character" className="character-small" />
-        <img
-          src="/img/dragon.png"
-          alt="character"
-          className="character-small"
-        />
-        <img src="/img/boy.png" alt="character" className="character-small" />
-        <img
-          src="/img/wizard.png"
-          alt="character"
-          className="character-small"
-        />
-        <img src="/img/woman.png" alt="character" className="character-small" />
+          <div>
+            <p className="center-text">Captain America</p>
+            <img
+              src="/img/superhero.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Pencil-man</p>
+            <img
+              src="/img/pencil.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Ninja-man</p>
+            <img
+              src="/img/ninja.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Hippie-man</p>
+            <img
+              src="/img/native-american.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Woke-man</p>
+            <img
+              src="/img/mythical-creature.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Ghost-man</p>
+            <img
+              src="/img/ghost.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Dragon-man</p>
+            <img
+              src="/img/dragon.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Man</p>
+            <img
+              src="/img/boy.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Wizard-man</p>
+            <img
+              src="/img/wizard.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
+          <div>
+            <p className="center-text">Wo-man</p>
+            <img
+              src="/img/woman.png"
+              alt="character"
+              className="character-small"
+            />
+          </div>
         </div>
       </main>
     </>
